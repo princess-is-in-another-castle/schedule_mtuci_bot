71af1a9ddc8e2e2b4ed8f75112a979e0b1ab3110
princess-is-in-another-castle <rus31344@gmail.com>
2021-04-12 19:37:44 +0300

    Убрал модуль, теперь в нём нет нужды

--- src/main/kotlin/UselessModules/ReadFiles.kt
+++ src/main/kotlin/UselessModules/ReadFiles.kt
@@ -1,16 +1,14 @@
-
 import java.io.File
 import java.io.FileInputStream
 import org.apache.poi.ss.usermodel.Workbook
 import org.apache.poi.ss.usermodel.WorkbookFactory
-import org.jetbrains.exposed.sql.selectAll
-import org.jetbrains.exposed.sql.transactions.transaction
-import Data.*
+
 
 /** Получает путь до папки с расписаниями.
  * По всем файлам в папке(а там должны быть только доки с расписанием)
  * ищет и добавляет группы в базу, ищет и добавляет расписание групп в базу.
- * Можно же сразу находить группу и добавлять её расписание? Можно, но это надо переписывать код.*/
+ * Можно же сразу находить группу и добавлять её расписание? Можно, но это надо переписывать код.
+ * Кстати, код переписан и теперь эта функция подлежит удалению*/
 fun readFiles(pathFolder: String) {
     // код для того, чтобы прочитать все имеющиеся файлы в папке
     val folder = File(pathFolder)
@@ -30,7 +28,8 @@ fun readFiles(pathFolder: String) {
 }
 
 /** Получает путь до файла расписания.
- * Добавляет группы в базу. */
+ * Добавляет группы в базу.
+ *  Уже не нужно*/
 fun putUsersInDb(filepath: String) {
     val input = FileInputStream(filepath)
     val workbook: Workbook = WorkbookFactory.create(input)
@@ -41,7 +40,8 @@ fun putUsersInDb(filepath: String) {
 }
 
 /** Получает путь до файла расписания.
- * Ищет группы в документе и добавляет их расписание в базу.*/
+ * Ищет группы в документе и добавляет их расписание в базу.
+ *  Уже не нужно*/
 fun putTimeTableInDb(filepath: String) {
     val input = FileInputStream(filepath)
     val workbook: Workbook = WorkbookFactory.create(input)
@@ -58,16 +58,27 @@ fun putTimeTableInDb(filepath: String) {
 
 }
 
-/** копирует список групп из базы данных и возвращает его.
- * Вообще костыль, потому что лучше искать по базе данных группу, а не копировать список групп. */
-fun getListOfGroups(): List<String> {
+/** Скачивает все файлы по url в папку
+ *  Функцию уже нет смысла использовать */
+fun getFiles(url: String, pathFolder: String) {
 
-    val groups = mutableListOf<String>()
-    transaction {
-        Groups.selectAll().forEach {
-            groups.add(it[Groups.groups])
+    deleteAllFilesInFolder(pathFolder)
+
+    val listNews = getHtmlAttributes(url)
+    // перебирает все элементы
+    for (element in listNews) {
+        // если расписание не для ЦЗОПБ и ЦЗОПМ
+        if (!(element.text().contains("ЦЗОПБ")) and
+            !(element.text().contains("ЦЗОПМ")) and
+            !(element.text().contains("СиСС - 11.04.02 (71,75)"))
+        ) { // просто бредовое расписание
+
+            // название файла. Ахтунг!, в названии файла указана сначала папка files/
+            val fileName = element.attr("href").removePrefix("files/")
+            //вывести ссылку название аттрибута в консоль
+            println(fileName)
+
+            getFile(url, fileName, pathFolder)
         }
     }
-
-    return groups.toList()
 }
