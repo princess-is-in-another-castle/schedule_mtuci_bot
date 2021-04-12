67ea5b907bd2917f21075d2e9deb5671ab4eb95a
princess-is-in-another-castle <rus31344@gmail.com>
2021-04-12 20:01:30 +0300

    Полностью переписал добавление инфы в базу данных

--- src/main/kotlin/DownloadFiles.kt
+++ src/main/kotlin/DownloadFiles.kt
@@ -1,67 +1,116 @@
 import org.apache.commons.io.FileUtils
+import org.apache.poi.ss.usermodel.Workbook
+import org.apache.poi.ss.usermodel.WorkbookFactory
 import org.jsoup.Jsoup
 import org.jsoup.select.Elements
 import java.io.File
+import java.io.FileInputStream
 import java.net.URL
 
-/** Скачивает все файлы по url в папку */
-fun getFiles(url: String, pathFolder: String) {
-
-    deleteAllFilesFolder(pathFolder)
-
-    val listNews = getHtmlAttributes(url)
-    // перебирает все элементы
-    for (element in listNews) {
-            // если расписание не для ЦЗОПБ и ЦЗОПМ
-            if (!(element.text().contains("ЦЗОПБ")) and
-                !(element.text().contains("ЦЗОПМ")) and
-                !(element.text().contains("СиСС - 11.04.02 (71,75)"))  ) { // просто бредовое расписание
-
-                // название файла. Ахтунг!, в названии файла указана сначала папка files/
-                val fileName = element.attr("href").removePrefix("files/")
-                //вывести ссылку название аттрибута в консоль
-                println(fileName)
-
-                getFile(url, fileName, pathFolder)
-            }
+/** Получает на вход ссылку на сайт и папку для временных файлов
+ *  Каждый файл скачивает, потом отправляет на обработку для добавления данных в дб, после обработки файл удаляется. */
+fun putDataInDb(url: String, pathFolder: String) {
+    // Список с именами файлов
+    val docNames = getDocNames(getHtmlAttributes(url))
+    docNames.forEach { docName ->
+        val fileUrl = getFileUrl(url, fileName = docName)
+        downloadFile(
+            fileUrl = fileUrl,
+            fileName = docName,
+            pathFolder = pathFolder
+        )
+        val folder = File(pathFolder)
+        folder.listFiles().forEach { doc ->
+            val input = FileInputStream(doc)
+            val workbook: Workbook = WorkbookFactory.create(input)
+            val sheet = workbook.getSheetAt(0)
+            getGroups(sheet)
         }
+        deleteAllFilesInFolder(pathFolder)
+
+    }
+
 }
 
-/** функция возращает элементы, имеющие в названии подстроку "Расписание занятий" */
+
+/** Получает ссылку на сайт и название файла.
+ *  Возвращает ссылку на файл на сайте для дальнейшего скачивания. */
+fun getFileUrl(url: String, fileName: String): String {
+    return url + "files/" + fileName
+}
+
+
+/** Получает на вход путь до папки и название файла.
+ *  Возвращает путь до файла в этой папке. */
+fun getPathToFile(pathFolder: String, fileName: String): String {
+    return pathFolder + fileName
+}
+
+
+/** Возвращает список названий документов */
+fun getDocNames(listOfElements: Elements): List<String> {
+    val namesOfDocs = mutableListOf<String>()
+    listOfElements.forEach { element ->
+        element.takeUnless{
+            // если расписание не для ЦЗОПБ, ЦЗОПМ и магистров 11.04.02 (71,75), расписание которых спарсить нормально не получится
+                    (it.text().contains("ЦЗОПБ")) or
+                    (it.text().contains("ЦЗОПМ")) or
+                    (it.text().contains("СиСС - 11.04.02 (71,75)"))
+        }?.apply {
+            // название файла. Ахтунг!, в названии файла указана вначале папка files/, поэтому удаляем её
+            val fileName = this.attr("href").removePrefix("files/")
+            //вывести название файла в консоль
+            println(fileName)
+            namesOfDocs.add(fileName)
+        }
+    }
+    return namesOfDocs.toList()
+}
+
+
+/** Качает файл в папку*/
+fun downloadFile(fileUrl: String, fileName: String, pathFolder: String) {
+    val filePath = getPathToFile(pathFolder, fileName)
+    FileUtils.copyURLToFile(
+        URL(fileUrl),
+        File(filePath)
+    )
+    println("Файл $fileName успешно скачан")
+}
+
+
+/** На входе получает ссылку на сайт.
+ * Возвращает элементы "href" c html страницы, имеющие в названии подстроку "Расписание занятий". */
 fun getHtmlAttributes(url: String): Elements {
-    // функция возращает элементы, имеющие в названии подстроку "Расписание занятий"
-    // парсит html страницу
     val doc = Jsoup.connect(url)
         .userAgent("Mozilla")
         .get()
-
     // достаёт все элементы, у которых аттрибут в названии имеет подстроку "Расписание занятий"
-    val listNews = doc.getElementsByAttributeValueContaining("href", "Расписание занятий")
-    return listNews
+    return doc.getElementsByAttributeValueContaining("href", "Расписание занятий")
 }
+
+
+/** очищает папку от файлов.*/
+fun deleteAllFilesInFolder(folderPath: String) {
+    File(folderPath).listFiles().forEach { file ->
+        file.delete()
+    }
+}
+
 
 /** на вход получает url сайта, имя файла и путь к папке.
  *  Закидывает файл в папку */
-fun getFile(url: String, fileName:String, pathFolder: String) {
+fun getFile(url: String, fileName: String, pathFolder: String) {
     // создаёт ссылку, где будет храниться файл (путь + название файла)
-    val pathFile = pathFolder + fileName
+    val pathToFile = getPathToFile(pathFolder, fileName)
     // создаёт ссылку на файл на сайте
-    val fileUrl = url + "files/" +fileName
+    val fileUrl = getFileUrl(url, fileName)
     // скачивает файл по url в папку
     FileUtils.copyURLToFile(
         URL(fileUrl),
-        File(pathFile)
+        File(pathToFile)
     )
-    println("файл успешно спизжен")
+    println("файл успешно скачан")
 }
 
-/** очищает папку от файлов.
- * Будет полезно, когда зальют новое расписание, а оно будет отличаться по названию от старого.*/
-fun deleteAllFilesFolder(path: String) {
-
-    val folder = File(path)
-    val listOfFiles = folder.listFiles()
-    for (myFile in listOfFiles)
-        if (myFile.isFile) myFile.delete()
-}
 
