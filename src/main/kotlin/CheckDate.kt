13d64690224e5069062ab119c47113bb821cbaad
princess-is-in-another-castle <rus31344@gmail.com>
2021-04-14 22:45:49 +0300

    подкорректировал

--- src/main/kotlin/CheckDate.kt
+++ src/main/kotlin/CheckDate.kt
@@ -4,10 +4,9 @@ import org.joda.time.DateTimeZone
 import org.joda.time.Weeks
 import java.time.DayOfWeek
 
-/** Возвзвращает номер текущей недели. Высчитывает, начиная с первого дня учебного дня семестра
+/** Возвращает номер текущей недели. Высчитывает, начиная с первого дня учебного дня семестра
  * Сейчас здесь прописано 1 февраля 2021г. */
 fun numberOfWeek(): Int {
-    // Возвзвращает номер текущей недели
     val myZone: DateTimeZone = DateTimeZone.forID("Europe/Moscow")
     DateTimeZone.setDefault(myZone)
     val dateTime1 = DateTime(2021, 2, 1, 0, 0)
